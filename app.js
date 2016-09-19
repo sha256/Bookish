f064c5d4a23d25a7fc20372808bff193fe92249a
Shamim Hasnath <sha404@live.com>
2016-09-20 04:10:27 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var {app, BrowserWindow} = require('electron');
+var myMenu = require('./js/menu.js');
+
+var win = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform == 'darwin') {
+    app.quit();
+  }
+});
+
+
+app.on('ready', function() {
+
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    'min-width': 500,
+    'min-height': 200,
+    'accept-first-mouse': true,
+    'title-bar-style': 'hidden'
+  });
+
+  myMenu();
+
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  //win.webContents.openDevTools();
+
+  win.on('closed', function() {
+
+    win = null;
+  });
+
+});
