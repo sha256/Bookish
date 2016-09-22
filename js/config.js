bb1f72f4ee5e63f58ed023a461293b78410a4d5e
Shamim Hasnath <sha404@live.com>
2016-09-22 09:14:29 +0600

    perfectly working CK, CM, Shared toolbar

---
+++ js/config.js
@@ -0,0 +1,26 @@
+
+CKEDITOR.editorConfig = function(cf){
+    cf.removePlugins = 'resize,about,maximize';
+    cf.extraPlugins= 'codemirror,sharedspace';
+    cf.sharedSpaces = { top: 'ckToolbar'};
+    cf.height = "100%";
+
+    cf.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	cf.removeButtons = 'About,Maximize,Preview';
+
+};
